84268d90355794d74e9187e4b5bd00bee54f83df
cpamungkas <necha10.02@gmail.com>
2021-12-30 01:11:59 +0700

    Update

--- application/config/email.php
+++ application/config/email.php
@@ -2,10 +2,10 @@
 
 $config = array(
     'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
-    'smtp_host' => 'ssl://smtp.googlemail.com', //'10.12.30.10', 
+    'smtp_host' => 'ssl://smtp.googlemail.com',
     'smtp_port' => 465,
-    'smtp_user' => 'necha10.02@gmail.com', //'redaksi@gramediapustakautama.id', //'itservicedesk@gramedia.com',
-    'smtp_pass' => 'rqjzzxkmyzehkuyh', //'1d427p8trb8347r', //'HDgr4m3d1470!',    
+    'smtp_user' => 'akun_email@gmail.com',
+    'smtp_pass' => 'password_app_gmail',
     'mailtype' => 'html', //plaintext 'text' mails or 'html'
     'smtp_timeout' => '4', //in seconds
     'charset' => 'iso-8859-1',
